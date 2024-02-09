e6726735050909561156814c5edc96ae5047c8e2
devfer <devfercastro@gmail.com>
2024-02-08 21:43:50 -0300

    Linear system solver test

--- main.py
+++ main.py
@@ -14,3 +14,27 @@ assert la.rank(C) == np.linalg.matrix_rank(C)
 
 D = np.array([[1, 2, 3], [0, 2, 6], [-1, -2, 3], [0, 0, 6], [2, 0, -6]])
 assert la.rank(D) == np.linalg.matrix_rank(D)
+
+
+# * Testing linear systems solver
+M = np.array([[1, -2, 3], [0, 1, -2], [0, 0, 1]])
+# s = np.array([[3], [-2], [1]])
+s = np.array([3, -2, 1])
+"""
+T = [
+    [1,-2,3],
+    [0,1,-2],
+    [0,0,1]
+]
+Ts = [
+    [3],
+    [-2],
+    [1]
+]
+
+Solution = (0,0,1)
+"""
+my_sol = la.sol_ls(M, s)
+np_sol = np.linalg.solve(M, s)
+
+assert np.all(my_sol == np_sol)
